e72950a2f55bf348b8e808a247be7dbcc2a08a27
Gage <cgz_0077@163.com>
2016-08-03 14:16:27 +0800

    创建工程，构建工程目录结构

---
+++ app/src/main/java/com/caitou/simpleweather/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.caitou.simpleweather.activities;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import com.caitou.simpleweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
